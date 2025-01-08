22b608633ab85615d800dc64418894d5ee05d32d
DevTec2020 <leonardo.silva.inf@gmail.com>
2025-01-07 23:31:20 -0300

    FEAT: Testando Layout para section Habilidades

--- src/components/Habilidades/index.tsx
+++ src/components/Habilidades/index.tsx
@@ -1,7 +1,25 @@
 export function Habilidades() {
     return (
-        <div className="flex flex-col text-white w-1/3">
-            <h1>Habilidades</h1>
+        <div className="flex flex-col min-h-screen w-full text-white bg-red-900">
+            <div className="flex flex-col items-center justify-center">
+                <div className="mt-20">
+                    <h1>Habilidades</h1>
+                </div>
+
+                <div className="flex mt-20 gap-32">
+                    <div>
+                        <h2>Frontend</h2>
+                    </div>
+
+                    <div>
+                        <h2>Backend</h2>
+                    </div>
+
+                    <div>
+                        <h2>Outros</h2>
+                    </div>
+                </div>
+            </div>
             
         </div>
     )
